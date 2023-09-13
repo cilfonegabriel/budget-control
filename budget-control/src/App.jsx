4f77d42fd1bbddd0e751b9b467087360659cda79
gabicilfone <cilfonegabriel@gmail.com>
2023-09-13 11:56:43 -0300

    Add components to the app file

--- budget-control/src/App.jsx
+++ budget-control/src/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
+import Header from './components/Header'
 
 function App() {
 
   return (
-    <h1>control de gastos</h1>
+    <h1>
+      <Header />
+    </h1>
   )
 }
 
