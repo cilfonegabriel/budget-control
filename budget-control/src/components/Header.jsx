20f7d4d84b476a1e97b59d6d60a1a40642c02499
gabicilfone <cilfonegabriel@gmail.com>
2023-09-14 17:41:07 -0300

    first way to the component of budget control

--- budget-control/src/components/Header.jsx
+++ budget-control/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import NewBudget from './NewBudget'
+import BudgetControl from './BudgetControl'
 
 const Header = ({
     budget,
@@ -12,7 +13,10 @@ const Header = ({
         <h1>Expense Planner</h1>
 
         {isValidBudget ? (
-            <p>Budget Control</p>
+            <BudgetControl 
+                budget={budget}
+
+            />
         ) : (
             <NewBudget
                 budget={budget}
