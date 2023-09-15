e9f267afb5714f23011c1df79470486c43d2f0ee
gabicilfone <cilfonegabriel@gmail.com>
2023-09-15 11:00:54 -0300

    Adding the expense form

--- budget-control/src/components/Modal.jsx
+++ budget-control/src/components/Modal.jsx
@@ -22,7 +22,51 @@ const Modal = ({setModal, animateModal, setAnimateModal}) => {
       </div>
 
       <form className={`formulario ${animateModal ? "animar" : 'cerrar'}`}>
-        <legend>New Spent</legend>
+        <legend>New expense</legend>
+
+        <div className='campo'>
+          <label htmlFor="nombre">Name Expense</label>
+          
+          <input 
+            id='name'
+            type="text" 
+            placeholder='Add the name of the expense'
+          />
+
+        </div>
+
+        <div className='campo'>
+          <label htmlFor="cantidad">Amount</label>
+          
+          <input 
+            id='amount'
+            type="number" 
+            placeholder='Add the amount of the expense: ej.300'
+          />
+          
+        </div>
+
+        <div className='campo'>
+          <label htmlFor="categoria">Category</label>
+
+          <select
+            id="category"
+          >
+            <option value="">-- Select</option>
+            <option value="saving">Saving</option>
+            <option value="meal">Meal</option>
+            <option value="house">House</option>
+            <option value="miscellaneous">Miscellaneous expenses</option>
+            <option value="leisure">Leisure</option>
+            <option value="health">Health</option>
+            <option value="subscriptions">Subscriptions</option>
+          </select>
+        </div>
+
+        <input 
+          type="submit" 
+          value="Add Expense" 
+        />
 
       </form>
     </div>
